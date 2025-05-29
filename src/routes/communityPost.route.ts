23aee1c5315dabd876ec0bf04909f25454863485
Mohak-Tripathi <mohaktripathi029@gmail.com>
2025-05-29 08:18:32 +0530

    communitypost create crud is done

--- src/routes/communityPost.route.ts
+++ src/routes/communityPost.route.ts
@@ -2,12 +2,20 @@ import { Router } from "express";
 import { CommunityPostController } from "../controllers/communityPost.controller";
 import { authMiddleware, authorize } from '../utils/authMiddleware';
 import { UserRole } from '@prisma/client';
+import { upload } from '../middlewares/upload.middleware';
 
 const router = Router();
 
 router.use(authMiddleware); 
 
-router.post("/", authorize(UserRole.SUPERADMIN, UserRole.ADMIN), CommunityPostController.create);
+// router.post("/", authorize(UserRole.SUPERADMIN, UserRole.ADMIN), CommunityPostController.create);
+
+router.post(
+    '/',
+    authorize(UserRole.SUPERADMIN, UserRole.ADMIN),
+    upload.array('images'), // 👈 Accept multiple images
+    CommunityPostController.create
+  );
 router.get("/",authorize(UserRole.SUPERADMIN, UserRole.ADMIN), CommunityPostController.getAll);
 router.get("/:id",authorize(UserRole.SUPERADMIN, UserRole.ADMIN), CommunityPostController.getById);
 router.put("/:id", authorize(UserRole.SUPERADMIN, UserRole.ADMIN), CommunityPostController.update);
